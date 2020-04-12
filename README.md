a49f7556084d691aa767dfc5ae463943e5a063e9
Danilo Bargen <mail@dbrgn.ch>
2020-04-12 02:05:40 +0200

    Allow controlling servo speed with potentiometer

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Servo control for the marble lift.
 
 Uses a SM-S4303 continuous rotation servo.
+
+## Wiring
+
+- `PA1` - Potentiometer (middle pin, the other two pins are connected to 3V3 and GND)
+- `PB9` - Servo data (white wire)
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,29 @@
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 
 use rtfm::app;
+use rtfm::cyccnt::U32Ext;
+use stm32f1xx_hal as hal;
+use stm32f1xx_hal::adc::Adc;
+use stm32f1xx_hal::gpio::{gpioa, Analog};
 use stm32f1xx_hal::pac;
 use stm32f1xx_hal::prelude::*;
+use stm32f1xx_hal::pwm::Pwm;
 use stm32f1xx_hal::time::Hertz;
 use stm32f1xx_hal::timer::Timer;
 
 // The main frequency in Hz
 const FREQUENCY: u32 = 48_000_000;
 
+// How often (in CPU cycles) the ADC should be polled
+const POLL_PERIOD: u32 = FREQUENCY / 10; // ~100ms
+
 #[app(device = stm32f1::stm32f103, peripherals = true, monotonic = rtfm::cyccnt::CYCCNT)]
 const APP: () = {
-    struct Resources { }
+    struct Resources {
+        adc: Adc<pac::ADC1>,
+        pin_adc: gpioa::PA1<Analog>,
+        pwm: Pwm<pac::TIM4, hal::pwm::C4>,
+    }
 
     /// Initialization happens here.
     ///
@@ -23,10 +35,10 @@ const APP: () = {
     /// access to Cortex-M and device specific peripherals through the `core`
     /// and `device` variables, which are injected in the scope of init by the
     /// app attribute.
-    #[init(spawn = [])]
-    fn init(ctx: init::Context) {
+    #[init(spawn = [poll_adc])]
+    fn init(ctx: init::Context) -> init::LateResources {
         // Cortex-M peripherals
-        let mut core: cortex_m::Peripherals = ctx.core;
+        let mut core: rtfm::Peripherals = ctx.core;
 
         // Device specific peripherals
         let device: pac::Peripherals = ctx.device;
@@ -34,7 +46,7 @@ const APP: () = {
         // Get reference to peripherals
         let mut rcc = device.RCC.constrain();
         let mut afio = device.AFIO.constrain(&mut rcc.apb2);
-        let gpioa = device.GPIOA.split(&mut rcc.apb2);
+        let mut gpioa = device.GPIOA.split(&mut rcc.apb2);
         let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
         let mut flash = device.FLASH.constrain();
 
@@ -53,19 +65,54 @@ const APP: () = {
             .pclk1(24.mhz())
             .freeze(&mut flash.acr);
 
+        // Set up PA1 as ADC input pin connected to a potentiometer
+        let pin_adc = gpioa.pa1.into_analog(&mut gpioa.crl);
+        let mut adc = Adc::adc1(device.ADC1, &mut rcc.apb2, clocks);
+        adc.default_cfg();
+
+        // Schedule polling timer for ADC
+        ctx.spawn.poll_adc().unwrap();
+
         // Set up PWM timer on pin PB9
         let pin_pwm = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
         let mut pwm = Timer::tim4(device.TIM4, &clocks, &mut rcc.apb1)
             .pwm(pin_pwm, &mut afio.mapr, 50.hz());
-
-        pwm.set_duty(pwm.get_max_duty() / 10);
+        pwm.set_duty(pwm.get_max_duty() / 200 * 15); // 1.5 ms by default
         pwm.enable();
+
+        // Assign resources
+        init::LateResources {
+            adc,
+            pin_adc,
+            pwm,
+        }
     }
 
     #[idle]
     fn idle(_: idle::Context) -> ! {
         // The idle loop
         loop {}
+    }
+
+    /// Regularly called task that polls the ADC and adjusts PWM duty cycle.
+    #[task(
+        resources = [adc, pin_adc, pwm],
+        schedule = [poll_adc],
+    )]
+    fn poll_adc(ctx: poll_adc::Context) {
+        // Read a 12-bit ADC value (0-4095).
+        let value: u16 = ctx.resources.adc.read(ctx.resources.pin_adc).unwrap();
+
+        // Adjust servo PWM duty cycle
+        // 0 -> 1ms
+        // 4096 -> 2ms
+        let duty = (ctx.resources.pwm.get_max_duty() as f32) / 20.0 * (1.0 + (value as f32) / 4096.0);
+        ctx.resources.pwm.set_duty(duty as u16);
+
+        // Re-schedule the timer interrupt
+        ctx.schedule
+            .poll_adc(ctx.scheduled + POLL_PERIOD.cycles())
+            .unwrap();
     }
 
     // RTFM requires that free interrupts are declared in an extern block when
