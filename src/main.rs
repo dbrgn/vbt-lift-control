a556d73af964b8f170f19011b20232c873f8433b
Danilo Bargen <mail@dbrgn.ch>
2020-04-12 02:25:05 +0200

    Create a Servo abstraction

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,11 @@ use rtfm::app;
 use rtfm::cyccnt::U32Ext;
 use stm32f1xx_hal as hal;
 use stm32f1xx_hal::adc::Adc;
-use stm32f1xx_hal::gpio::{gpioa, Analog};
+use stm32f1xx_hal::gpio::{gpioa, gpiob, Alternate, Analog, PushPull};
 use stm32f1xx_hal::pac;
 use stm32f1xx_hal::prelude::*;
 use stm32f1xx_hal::pwm::Pwm;
+use stm32f1xx_hal::rcc::Clocks;
 use stm32f1xx_hal::time::Hertz;
 use stm32f1xx_hal::timer::Timer;
 
@@ -21,12 +22,58 @@ const FREQUENCY: u32 = 48_000_000;
 // How often (in CPU cycles) the ADC should be polled
 const POLL_PERIOD: u32 = FREQUENCY / 10; // ~100ms
 
+pub struct Servo {
+    pwm: Pwm<pac::TIM4, hal::pwm::C4>,
+}
+
+impl Servo {
+    /// Create a new servo instance.
+    pub fn new(
+        pin: gpiob::PB9<Alternate<PushPull>>,
+        tim: pac::TIM4,
+        clocks: &Clocks,
+        apb1: &mut hal::rcc::APB1,
+        mapr: &mut hal::afio::MAPR,
+    ) -> Self {
+        let mut pwm = Timer::tim4(tim, &clocks, apb1).pwm(pin, mapr, 50.hz());
+        pwm.set_duty(pwm.get_max_duty() / 200 * 15); // 1.5 ms by default, should turn off
+        Self { pwm }
+    }
+
+    /// Enable the servo.
+    pub fn enable(&mut self) {
+        self.pwm.enable();
+    }
+
+    /// Disable the servo.
+    pub fn disable(&mut self) {
+        self.pwm.disable();
+    }
+
+    /// Set the speed from an ADC (12-bit) measurement.
+    pub fn speed_from_adc(&mut self, value: u16) {
+        // Clamp value
+        let val = if value > 4095 {
+            4095.0
+        } else {
+            value as f32
+        };
+
+        // Calculate servo PWM duty cycle:
+        //    0 -> 1ms
+        // 4096 -> 2ms
+        let duty = (self.pwm.get_max_duty() as f32) / 20.0 * (1.0 + val / 4096.0);
+
+        self.pwm.set_duty(duty as u16);
+    }
+}
+
 #[app(device = stm32f1::stm32f103, peripherals = true, monotonic = rtfm::cyccnt::CYCCNT)]
 const APP: () = {
     struct Resources {
         adc: Adc<pac::ADC1>,
         pin_adc: gpioa::PA1<Analog>,
-        pwm: Pwm<pac::TIM4, hal::pwm::C4>,
+        servo: Servo,
     }
 
     /// Initialization happens here.
@@ -75,16 +122,13 @@ const APP: () = {
 
         // Set up PWM timer on pin PB9
         let pin_pwm = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
-        let mut pwm = Timer::tim4(device.TIM4, &clocks, &mut rcc.apb1)
-            .pwm(pin_pwm, &mut afio.mapr, 50.hz());
-        pwm.set_duty(pwm.get_max_duty() / 200 * 15); // 1.5 ms by default
-        pwm.enable();
+        let servo = Servo::new(pin_pwm, device.TIM4, &clocks, &mut rcc.apb1, &mut afio.mapr);
 
         // Assign resources
         init::LateResources {
             adc,
             pin_adc,
-            pwm,
+            servo,
         }
     }
 
@@ -96,18 +140,15 @@ const APP: () = {
 
     /// Regularly called task that polls the ADC and adjusts PWM duty cycle.
     #[task(
-        resources = [adc, pin_adc, pwm],
+        resources = [adc, pin_adc, servo],
         schedule = [poll_adc],
     )]
     fn poll_adc(ctx: poll_adc::Context) {
         // Read a 12-bit ADC value (0-4095).
         let value: u16 = ctx.resources.adc.read(ctx.resources.pin_adc).unwrap();
 
-        // Adjust servo PWM duty cycle
-        // 0 -> 1ms
-        // 4096 -> 2ms
-        let duty = (ctx.resources.pwm.get_max_duty() as f32) / 20.0 * (1.0 + (value as f32) / 4096.0);
-        ctx.resources.pwm.set_duty(duty as u16);
+        // Adjust servo speed
+        ctx.resources.servo.speed_from_adc(value);
 
         // Re-schedule the timer interrupt
         ctx.schedule
